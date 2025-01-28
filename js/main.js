cc1b3ec019f9b233b266a54d19af19797987877f
OlgaML <omolina@geekcowsd.com>
2025-01-28 14:26:29 +0100

    Slime is falling and moving, gives error on slime.js:61

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const player = document.getElementById('player');
 const gameSpace = document.getElementById('game-container');
-
+const slime = new SlimeObstacle();
 
 
 // this gives a chance to add powerups in the future
@@ -16,45 +16,12 @@ document.addEventListener('keydown', (event)=>{
 })
 
 
-// Falling Slime
-
-
-function slimeDrop() {
-    let altitud = 0;
-    // Create new slime
-    const newSlime = document.createElement('div');
-    newSlime.classList.toggle("slime");
-    gameSpace.appendChild(newSlime);
-
-    // random initialPosition
-    let random = Math.floor(Math.random() * 100 )
-    let randomSpeed = Math.floor(Math.random() * 10 )
-    newSlime.style.left = `${random}%`;
-    // slime starting to fall
-    let fallingSlime = setInterval(() => {
-            console.log(altitud)
-            console.log(gameSpace.offsetHeight )
-        if( altitud < gameSpace.offsetHeight - 100 ) {
-            altitud ++;
-            newSlime.style.top = `${altitud}px`
-        } else {
-            clearInterval(fallingSlime);
-            slimeDrop();
-            gameSpace.removeChild(newSlime);
-        }
-        
-    }, randomSpeed)
-
-
-
-}
 
 
 
 // calling functions to start the game
-slimeDrop();
-slimeDrop();
-slimeDrop();
-slimeDrop();
+
+slime.dropSlime();
+slime.runSlime();
 
 
--- js/slime.js
+++ js/slime.js
@@ -1,7 +1,73 @@
-class SlimeObstacle{
-    constructor(top,align){
-        this.top = '5px';
-        this.left = '50px'
+class SlimeObstacle {
+    constructor() {
+        this.newSlime = document.createElement('div');
+        this.currentPosition = 0;
     }
+
+    // Falling Slime
+
+
+    dropSlime() {
+        let altitud = 0;
+        // Create new slime
+        this.newSlime = document.createElement('div');
+
+        this.newSlime.classList.toggle("slime");
+        gameSpace.appendChild(this.newSlime);
+
+        // random initialPosition
+        this.currentPosition = Math.floor(Math.random() * 100)
+        let randomSpeed = Math.floor(5, Math.random() * 10)
+        this.newSlime.style.left = `${this.currentPosition}%`;
+        // slime starting to fall
+        let fallingSlime = setInterval(() => {
+            console.log(altitud)
+            console.log(gameSpace.offsetHeight)
+            if (altitud < gameSpace.offsetHeight - 100) {
+                altitud++;
+                this.newSlime.style.top = `${altitud}px`
+            } else {
+                clearInterval(fallingSlime);
+                this.runSlime();
+                return;
+                //this.dropSlime();
+            }
+
+        }, randomSpeed)
+
+    }
+
+
+    // Slime movement on floor
+
+    runSlime() {
+        let slimeSteps = 0
+        let direction = Math.random();
+        const runDirection = setInterval(()=>{
+            if (direction < 0.5) {
+                this.currentPosition ++; // Move left
+            } else {
+                this.currentPosition --; // Move right
+            }
+            slimeSteps ++;
+            // Ensure it stays between 0% and 100%
     
+            // this.currentPosition = Math.min(100, Math.max(0, this.currentPosition + randomDirection)
+            // ); 
+            if (slimeSteps < 8){
+                this.newSlime.style.left = `${this.currentPosition}%`;
+            } else {
+                clearInterval(runDirection);
+                gameSpace.removeChild(this.newSlime);
+                
+            }
+            
+
+        },1000)
+
+
+
+
+    }
+
 };
